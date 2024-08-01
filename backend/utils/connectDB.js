996a0d13469e956bed79bf9d10ca4104606bda1e
MartyPR <martin.p1131@gmail.com>
2024-08-01 10:24:54 -0500

    Update connectDB.js

--- backend/utils/connectDB.js
+++ backend/utils/connectDB.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
-//pas:UYTO4HfU6qa8VlvP
+//pas:
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://martypr3:UYTO4HfU6qa8VlvP@ia-mern.maij9ws.mongodb.net/?retryWrites=true&w=majority&appName=IA-Mern");
+    const conn = await mongoose.connect(process.env.MONGODB);
     console.log(`Mongoose connected ${conn.connection.host}`);
   } catch (error) {
     console.error(`error connecting to MongoDB ${error}`);
